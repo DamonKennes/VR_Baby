e6ff992fb7398a26d2187eda6f8156876dd38b6a
Jens Sprengers <jenssprengers@gmail.com>
2024-04-27 22:51:04 +0200

    changing between scenes while keeping a gameobject intact + start screen UI to start a full user study or manually load a certain scene

--- Assets/ExperimentManager.cs
+++ Assets/ExperimentManager.cs
@@ -1,38 +1,93 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class ExperimentManager : MonoBehaviour
 {
-    private AssetBundle myLoadedAssetBundle;
-    private string[] scenePaths;
-    private int currentScene;
+    // environments is a list of names of scenes
+    public List<string> environments;
+    // characters are gameobjects to be added to the environment scene
+    public List<GameObject> characters;
+    // weapons are gameobjects to be added to the environment scene
+    public List<GameObject> weapons;
 
-    // Start is called before the first frame update
-    void Start()
+    private List<string> subjectData;
+
+    // ui stuff
+    private UIDocument adminDocument;
+    private Button loadSceneButton;
+    private DropdownField loadSceneField;
+
+
+    private void Awake()
     {
-        currentScene = 0;
-        //myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Environments");
-        //scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+        // mark this gameObject to not get destroyed when another scene loads
+        DontDestroyOnLoad(this.gameObject);
+
+        // get ui elements
+        adminDocument = GetComponent<UIDocument>();
+        
+        loadSceneButton = adminDocument.rootVisualElement.Q("LoadSceneButton") as Button;
+        loadSceneField = adminDocument.rootVisualElement.Q("LoadSceneField") as DropdownField;
+        loadSceneButton.RegisterCallback<ClickEvent>((_) => {
+            LoadScene(loadSceneField.text); 
+        });
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            //currentScene %= scenePaths.Length - 1;
-            //Debug.Log("scene2 loading: " + scenePaths[currentScene]);
-            SceneManager.LoadScene(UnityEngine.Random.Range(0, 2), LoadSceneMode.Single);
-           // currentScene++;
-            //currentScene %= 2;
-
-
-
-
-
-        }
     }
 
+    // loads a new scene by name
+    public void LoadScene(string sceneName) 
+    {
+        // disable the ui
+        adminDocument.enabled = false;
+        // load scene
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    // generates a scene and loads it based on given sceneData
+    // sceneData is a string of three characters, character 0 is the index of the environment, character 1 is the index of the character, character 2 is the index of the weapon
+    public void GenerateAndLoadScene(string sceneData) 
+    {
+        if (sceneData.Length != 3) {
+            Debug.LogError("Invalid scene data");
+            return;
+        }
+        LoadScene(environments[Convert.ToInt32(sceneData[0])]);
+        Instantiate(characters[Convert.ToInt32(sceneData[1])]);
+        Instantiate(weapons[Convert.ToInt32(sceneData[2])]);
+    }
+
+    // start the user study
+    public void StartUserStudy(string groupId) 
+    {
+        // example
+        string groupA1 = "000+010+103+201";
+        subjectData = groupA1.Split('+').ToList<string>();
+        GenerateAndLoadScene(subjectData[0]);
+        subjectData.RemoveAt(0);
+    }
+
+    // continue user study: load next scene after task is completed
+    public void ContinueUserStudy() 
+    {
+        // collect and store results etc
+
+        if(subjectData.Count == 0)
+        {
+            // finished
+            //LoadEndScreenScene();
+            //return;
+        }
+        // load next scene
+        GenerateAndLoadScene(subjectData[0]);
+        subjectData.RemoveAt(0);
+    }
 
 }
